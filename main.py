4b1b20d17bc38a6c244b3dde56f88bdfb7b60df1
Tiphaine Viard <tiphaine.viard@gmail.com>
2015-07-20 17:49:51 +0200

    Removed resurrect option

--- main.py
+++ main.py
@@ -10,21 +10,12 @@ nb_lines = 0
 resurrect = False
 
 # Read arguments from command line
-if len(sys.argv) == 3 and "resurrect" not in sys.argv[1]:
+if len(sys.argv) == 2:
     delta = int(sys.argv[1])
-    treshold = float(sys.argv[2])
-
-elif len(sys.argv) == 4:
-    if "resurrect" in sys.argv[2]:
-        resurrect = True
-        delta = int(sys.argv[1])
 else:
     sys.stderr.write(
-        "Usage: cat <stream> | python main.py <delta> <treshold> [--resurrect=<err_file>]\
+        "Usage: cat <stream> | python main.py <int:delta>\
         \n\n")
-    sys.stderr.write("  --resurrect: Recover previous instance by providing\
-    an stderr output \
-    previous instance.\n\n")
     sys.exit(1)
 
 # Read stream
@@ -38,8 +29,7 @@ for line in sys.stdin:
     time = (t, t)
 
     # This a new instance
-    if not resurrect:
-        Cm.addClique(Clique((link, time), set([])))
+    Cm.addClique(Clique((link, time), set([])))
 
     # Populate data structures
     if link not in times:
@@ -58,50 +48,6 @@ for line in sys.stdin:
 Cm._times = times
 Cm._nodes = nodes
 sys.stderr.write("Processed " + str(nb_lines) + " from stdin\n")
-
-# Populate CliqueManager from err file
-if resurrect:
-    with open(sys.argv[3]) as error_file:
-        for line in error_file:
-            if line[0] == 'G':
-                # Remove from S
-                clique = line.split(" ")[2] + " " + line.split(" ")[3]
-                nodes = line.split(" ")[2].split(",")
-                duration = line.split(" ")[3].split("(")[0].split(",")
-                c = Clique(
-                    (frozenset(nodes), (int(
-                        duration[0]), int(
-                        duration[1]))))
-
-                if c in Cm._S:
-                    Cm._S.remove(c)
-
-            elif line[0] == 'A':
-                # Add to S and S_set
-                clique = line.split(" ")[1] + " " + line.split(" ")[2]
-                nodes = line.split(" ")[1].split(",")
-                duration = line.split(" ")[2].split(",")
-                c = Clique(
-                    (frozenset(nodes), (int(
-                        duration[0]), int(
-                        duration[1]))))
-
-                Cm._S.append(c)
-                Cm._S_set.add(c)
-
-            elif "maximal" in line:
-                # Add to R
-                clique = line.split(" ")[0] + " " + line.split(" ")[1]
-                nodes = line.split(" ")[0].split(",")
-                duration = line.split(" ")[1].split(",")
-                c = Clique(
-                    (frozenset(nodes), (int(
-                        duration[0]), int(
-                        duration[1]))))
-                Cm._R.add(c)
-
-            # Write original err line to be consistent
-            sys.stderr.write(line)
 
 # Restart execution
 R = Cm.getDeltaCliques(delta, treshold)
